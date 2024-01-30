9fd4ffc28bfcd68a7fe4fdf77b7f9a10604753fa
Garik <garikmartikyan03@gmail.com>
2024-01-30 17:14:26 +0400

    Sidebar's Toglle Shortcut

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import classes from "./App.module.scss";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
+  console.log("app render");
   const [sidebarOpen, setSidebarOpen] = useState(true);
+  const [title, setTitle] = useState("Close");
+
   let toggle;
   let sidebar;
 
@@ -11,8 +14,34 @@ function App() {
     sidebar = classes.sidebar;
   } else {
     toggle = classes.openBtn;
-    sidebar = classes.sidebar + " " + classes.close;
+    sidebar = `${classes.sidebar} ${classes.close}`;
   }
+
+  useEffect(() => {
+    const operatingSystem = navigator.platform;
+    if (
+      operatingSystem.toLowerCase().includes("win") ||
+      operatingSystem.toLowerCase().includes("linux")
+    ) {
+      setTitle("Toggle Sidebar - Ctrl ⇧ S");
+    } else if (operatingSystem.toLowerCase().includes("mac")) {
+      setTitle("Toggle Sidebar - ⌘ ⇧ S");
+    } else {
+      setTitle("Toggle Sidebar");
+    }
+
+    document.addEventListener("keydown", function (event) {
+      const isPressedCmdOrCtrl = event.ctrlKey || event.metaKey;
+      const isPressedShift = event.shiftKey;
+      const isPressedS = event.key.toUpperCase() === "S";
+      const SidebarToggleShortcut =
+        isPressedCmdOrCtrl && isPressedShift && isPressedS;
+
+      if (SidebarToggleShortcut) {
+        setSidebarOpen((prv) => !prv);
+      }
+    });
+  }, []);
 
   return (
     <>
@@ -20,8 +49,8 @@ function App() {
       <div className={classes.main}>
         <div
           className={toggle}
-          onClick={() => setSidebarOpen((prv) => !prv)}
-          title=""
+          onClick={() => setSidebarOpen((prev) => !prev)}
+          title={title}
         >
           <div className={classes.pt1}></div>
           <div className={classes.pt2}></div>
