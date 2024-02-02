76adfbe176dc1b4aa35326c88b0c49fe593a4c2f
Garik <garikmartikyan03@gmail.com>
2024-02-03 00:25:26 +0400

    Style Structure Optimizing

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -4,23 +4,21 @@ import ChatTitle from "./ChatTitle/ChatTitle.jsx";
 
 export default function Sidebar({ chats }) {
   return (
-    <div className={classes.wrapper}>
-      <div className={classes.container}>
-        <div className={classes.header}>
-          <h1>AI Chat</h1>
-          <button title="New Chat">
-            <MessageSquarePlus />
-          </button>
-        </div>
-        <section className={classes.chats}>
-          <h1>Chats</h1>
-          <div className={classes.chatContainer}>
-            {new Array(15).fill("").map((item, index) => (
-              <ChatTitle key={index} />
-            ))}
-          </div>
-        </section>
+    <div className={classes.container}>
+      <div className={classes.header}>
+        <h1>AI Chat</h1>
+        <button title="New Chat">
+          <MessageSquarePlus />
+        </button>
       </div>
+      <section className={classes.chats}>
+        <h1>Chats</h1>
+        <div className={classes.chatContainer}>
+          {new Array(15).fill("").map((item, index) => (
+            <ChatTitle key={index} />
+          ))}
+        </div>
+      </section>
     </div>
   );
 }
